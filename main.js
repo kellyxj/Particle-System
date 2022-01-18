d0cf77be3352770019cfab6274b623e8fa6c774f
Kelly Jiang <kllyjiang@gmail.com>
2022-01-18 09:03:17 -0600

    adding some js boilerplate

---
+++ main.js
@@ -0,0 +1,81 @@
+// Vertex shader program----------------------------------
+var glsl = require("glslify");
+var VSHADER_SOURCE = glsl.file("./shaders/vertexShader.vert");
+
+// Fragment shader program----------------------------------
+var FSHADER_SOURCE = glsl.file("./shaders/fragmentShader.frag");
+
+var glsl = require("glslify");
+var VSHADER_SOURCE = glsl.file("./shaders/vertexShader.vert");
+
+// Fragment shader program----------------------------------
+var FSHADER_SOURCE = glsl.file("./shaders/fragmentShader.frag");
+
+function main() {
+//==============================================================================
+  // Retrieve <canvas> element
+  var canvas = document.getElementById('webgl');
+
+  // Get the rendering context for WebGL
+  var gl = getWebGLContext(canvas);
+  if (!gl) {
+    console.log('Failed to get the rendering context for WebGL');
+    return;
+  }
+
+  // Initialize shaders
+  if (!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)) {
+    console.log('Failed to intialize shaders.');
+    return;
+  }
+
+  // 
+  var n = initVertexBuffer(gl);
+  if (n < 0) {
+    console.log('Failed to set the vertex information');
+    return;
+  }
+
+  // Specify the color for clearing <canvas>
+  gl.clearColor(0.0, 0.0, 0.0, 1.0);
+
+	// NEW!! Enable 3D depth-test when drawing: don't over-draw at any pixel 
+	// unless the new Z value is closer to the eye than the old one..
+//	gl.depthFunc(gl.LESS);			 // WebGL default setting: (default)
+	gl.enable(gl.DEPTH_TEST); 	 
+	 
+//==============================================================================
+// STEP 4:   REMOVE This "reversed-depth correction"
+//       when you apply any of the 3D camera-lens transforms: 
+//      (e.g. Matrix4 member functions 'perspective(), frustum(), ortho() ...)
+//======================REVERSED-DEPTH Correction===============================
+
+  //  b) reverse the usage of the depth-buffer's stored values, like this:
+  gl.enable(gl.DEPTH_TEST); // enabled by default, but let's be SURE.
+  gl.clearDepth(0.0);       // each time we 'clear' our depth buffer, set all
+                            // pixel depths to 0.0  (1.0 is DEFAULT)
+  gl.depthFunc(gl.GREATER);
+
+  var u_ModelMatrix = gl.getUniformLocation(gl.program, 'u_ModelMatrix');
+  if (!u_ModelMatrix) { 
+    console.log('Failed to get the storage location of u_ModelMatrix');
+    return;
+  }
+  // Create a local version of our model matrix in JavaScript 
+  var modelMatrix = new Matrix4();
+
+  var u_MvpMatrix = gl.getUniformLocation(gl.program, 'u_MvpMatrix');
+  if (!u_MvpMatrix) { 
+    console.log('Failed to get the storage location of u_MvpMatrix');
+    return;
+  }
+
+  var mvpMatrix = new Matrix4();
+
+  var tick = function() {
+	drawAll(gl, n, currentAngle, modelMatrix, u_ModelMatrix, mvpMatrix, u_MvpMatrix); 
+	
+    requestAnimationFrame(tick, canvas);   
+  };
+  tick();		
+}
